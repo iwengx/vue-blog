04033ec0731808a74450373670978054b164da0b
wengx-unx <1027382710@qq.com>
2022-05-11 00:39:55 +0800

    feat(code-editor): created a code editor component

---
+++ src/components/code-editor.vue
@@ -0,0 +1,42 @@
+<template>
+   <prism-editor
+      class="my-editor"
+      v-model="editorCode"
+      :highlight="highlighter"
+      line-numbers
+   ></prism-editor>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+
+const { code } = defineProps({
+   code: String,
+});
+
+let editorCode = ref(code);
+
+const highlighter = (code: string) => {
+   return prism.highlight(code, prism.languages.js, 'js');
+};
+</script>
+
+<style lang="scss" scoped>
+.my-editor {
+   background: #2d2d2d;
+   color: #ccc;
+   font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+   font-size: 16px;
+   line-height: 1.5;
+   padding: 12px 5px;
+   border-radius: 10px;
+}
+
+::v-deep(.prism-editor__textarea:focus) {
+   outline: none;
+}
+</style>
