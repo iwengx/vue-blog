614d0eb74ea28355ad4f6389d6343ebc55c169ae
wengx-unx <1027382710@qq.com>
2022-05-11 00:41:29 +0800

    perf(right-content): replace code-editor scheme

--- src/components/right-content.vue
+++ src/components/right-content.vue
@@ -1,11 +1,21 @@
 <template>
-   <div id="master" class="md-body" v-html="contentHTML"></div>
+   <div class="master">
+      <template v-for="(item, index) in contentHTML">
+         <template v-if="index % 2 === 0">
+            <div v-html="item"></div>
+         </template>
+         <template v-else>
+            <base-code-editor :code="item"></base-code-editor>
+         </template>
+      </template>
+   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import { memoize } from 'lodash';
+import CodeEditor from './code-editor.vue';
 import showdown from 'showdown';
 import axios from 'axios';
 
@@ -18,43 +28,52 @@ const fetchPost = memoize(async (url: string) => {
    const markdown = response.data;
 
    // 编译 markdown 并给 a 标题添加 target="_blank" 属性
-   let html: string = converter
+   const html: string = converter
       .makeHtml(markdown)
       .replaceAll('<a href=', '<a target="_blank" href=');
 
-   // 单独给 pre 标签内的代码关键字添加带有 class 的 span 标签
-   let preIndex = html.indexOf('<pre>');
-   if (preIndex) {
-      let splitArr = html.split('<pre>');
-      for (let i = 1; i < splitArr.length; i++) {
-         splitArr[i] =
-            splitArr[i]
-               .split('</pre>')[0]
-               .replaceAll('const', '<span class="const">const</span>')
-               .replaceAll('function', '<span class="function">function</span>')
-               .replaceAll('router', '<span class="router">router</span>')
-               .replaceAll('axios', '<span class="axios">axios</span>')
-               .replaceAll('return', '<span class="return">return</span>')
-               .replaceAll('string', '<span class="string">string</span>')
-               .replaceAll('import', '<span class="import">import</span>')
-               .replaceAll('from', '<span class="from">from</span>')
-               .replaceAll('setup', '<span class="setup">setup</span>')
-               .replaceAll('mounted', '<span class="mounted">mounted</span>')
-               .replaceAll('updated', '<span class="updated">updated</span>')
-               .replaceAll('createApp', '<span class="createApp">createApp</span>') +
-            '</pre>' +
-            splitArr[i].split('</pre>')[1];
-      }
-      html = splitArr.join('<pre>');
-   }
+   let splitFullArr: string[] = [];
 
-   return html;
+   const preIndex = html.indexOf('<pre>');
+   // 判断当前内容是否存在 pre 标签（是否存在代码）
+   if (preIndex) {
+      // 将内容以 <pre> 标签分割
+      let splitPre = html.split('<pre>');
+
+      // 第0项是除了 <pre> 标签以外的内容，直接添加到 splitFullArr 中
+      splitFullArr.push(splitPre[0]);
+
+      // 从第1项开始，以 </pre> 结束标签再次分割，得到代码块和内容块
+      for (let i = 1; i < splitPre.length; i++) {
+         splitFullArr.push(...splitPre[i].split('</pre>'));
+      }
+      // 此时得到的 splitFullArr 数组中，偶数是内容块，奇数是代码块
+
+      // 去除代码块中的 <code></code> 标签
+      for (let j = 1; j < splitFullArr.length; j += 2) {
+         let code = splitFullArr[j];
+         // 去除 <code> 标签
+         code = code.substring(code.indexOf('>') + 1);
+         // 去除 </code> 标签
+         code = code.substring(0, code.length - 8);
+         // 重新赋值回去
+         splitFullArr[j] = code;
+      }
+   }
+   // 不存在则直接返回
+   else {
+      splitFullArr.push(html);
+   }
+   return splitFullArr;
 });
 
 export default defineComponent({
+   components: {
+      'base-code-editor': CodeEditor,
+   },
    setup() {
       const route = useRoute();
-      const contentHTML = ref<string>();
+      const contentHTML = ref<string[]>();
 
       watchEffect(async () => {
          const path = route.path === '/' ? '/demo/home.md' : route.path;
@@ -69,7 +88,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#master {
+.master {
    color: var(--text-color);
    line-height: 30px;
    max-width: 1440px;
@@ -102,45 +121,6 @@ export default defineComponent({
       border-left: 0.2rem solid var(--blockquote-border-color);
       margin: 1rem 0;
       padding: 0.25rem 0 0.25rem 1rem;
-   }
-
-   pre {
-      padding: 16px;
-      overflow: auto;
-      font-size: 85%;
-      line-height: 1.45;
-      background-color: var(--pre-background-color);
-      border-radius: 6px;
-
-      code {
-         color: #e0e0e0;
-         font-size: 1.2em;
-         font-family: Consolas;
-      }
-
-      .function,
-      .const {
-         color: #2889d9;
-      }
-      .return {
-         color: #c586c0;
-      }
-      .string {
-         color: #4ec9b0;
-      }
-      .router {
-         color: #4fc1ff;
-      }
-      .import,
-      .from {
-         color: #c586c0;
-      }
-      .createApp,
-      .mounted,
-      .updated,
-      .setup {
-         color: #dcdcaa;
-      }
    }
 }
 </style>
